9c556a131e7417b5e291c97b826f2c33514086d8
钟志程 <zhongzhicheng1@bluemoon.com.cn>
2023-09-05 14:45:31 +0800

    蜗牛爬山，有一点是一点

--- hs_chat_service/src/main/java/cn/com/wanshi/chat/common/aspect/ValidateTokenAspect.java
+++ hs_chat_service/src/main/java/cn/com/wanshi/chat/common/aspect/ValidateTokenAspect.java
@@ -14,7 +14,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;

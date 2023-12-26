d2472c3cd024bc0eeab8fc699aefc9045feb1fba
钟志程 <zhongzhicheng1@bluemoon.com.cn>
2023-12-26 13:53:57 +0800

    抄来的代码先提交

--- hs_chat_service/src/main/java/cn/com/wanshi/chat/group/model/req/GroupInitReq.java
+++ hs_chat_service/src/main/java/cn/com/wanshi/chat/group/model/req/GroupInitReq.java
@@ -1,9 +1,35 @@
 package cn.com.wanshi.chat.group.model.req;
 
+import cn.com.wanshi.chat.common.model.BaseReq;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.List;
+
 /**
  * @author zzc
  */
-public class GroupInitReq {
+@Data
+public class GroupInitReq extends BaseReq {
+
+
+    /**
+     * 群名称
+     */
+    @ApiModelProperty(value = "群名称")
+    private String groupName;
+
+
+    /**
+     * 群成员 是userId集合
+     */
+    @ApiModelProperty(value = "群成员 是userId集合")
+    private List<String> GroupMembers;
+
+
+
+
+
 
 
 
--- hs_chat_service/src/main/java/cn/com/wanshi/chat/group/model/resp/GroupInitResp.java
+++ hs_chat_service/src/main/java/cn/com/wanshi/chat/group/model/resp/GroupInitResp.java
@@ -1,4 +1,23 @@
 package cn.com.wanshi.chat.group.model.resp;
 
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * @author admin
+ */
+@Data
+@Builder
 public class GroupInitResp {
+
+
+    /**
+     * 群名称
+     */
+    @ApiModelProperty(value = "群名称")
+    private String groupName;
+
+
 }
